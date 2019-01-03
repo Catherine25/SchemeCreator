1f9192b84f62143fb6ed973138f828747028f01a
Catherine <Catherine25@users.noreply.github.com>
2019-01-03 22:50:19 +0200

    Added LineInfo class

---
+++ Data/LineInfo.cs
@@ -0,0 +1,15 @@
+﻿using Windows.Foundation;
+
+namespace SchemeCreator.Data
+{
+    public class LineInfo
+    {
+        public Point point1, point2;
+
+        public LineInfo(Point p1, Point p2)
+        {
+            point1 = p1;
+            point2 = p2;
+        }
+    }
+}
