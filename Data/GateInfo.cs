76b51c792a0bdfde843deb2a66290bca31eda3b3
Catherine <Catherine25@users.noreply.github.com>
2019-01-03 22:50:00 +0200

    Added GateInfo class

---
+++ Data/GateInfo.cs
@@ -0,0 +1,18 @@
+﻿using Windows.Foundation;
+
+namespace SchemeCreator.Data
+{
+    public class GateInfo
+    {
+        public Point point;
+        public string gateName;
+        public int newGateInputs;
+
+        public GateInfo(Point p, string name, int inputs)
+        {
+            point = p;
+            gateName = name;
+            newGateInputs = inputs;
+        }
+    }
+}
