a82219658369473cbbef341d9c94cf81aeb26bdd
Catherine <Catherine25@users.noreply.github.com>
2019-03-26 23:43:11 +0200

    Refactoring; Add event handlers: - NewGateBtClickedEvent; - SizeChanged; WorkSchemeEvent temporary empty; LastClickedBtEventArgs -> LastClickedButtonEventArgs;

--- UI/FrameManager.cs
+++ UI/FrameManager.cs
@@ -1,7 +1,6 @@
-﻿using System.Linq;
+﻿using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Shapes;
 
 namespace SchemeCreator.UI {
     class FrameManager {
@@ -24,8 +23,9 @@ namespace SchemeCreator.UI {
         }
 
         /*      methods     */
+        
         private void EventSubscribe() {
-            workspaceController.DotTappedEvent += DotTappedEvent;
+
             menuController.NewSchemeBtClickEvent += NewSchemeEvent;
             menuController.LoadSchemeBtClickEvent += LoadSchemeEvent;
             menuController.SaveSchemeBtClickEvent += SaveSchemeEvent;
@@ -34,17 +34,27 @@ namespace SchemeCreator.UI {
             menuController.AddGateBtClickEvent += AddGateEvent;
             menuController.AddLineBtClickEvent += AddLineEvent;
             menuController.RemoveLineBtClickEvent += RemoveLineEvent;
+            workspaceController.DotTappedEvent += DotTappedEvent;
+            newGateMenuController.NewGateBtClickedEvent += NewGateBtClickedEvent;
         }
+
         public Constants.FrameEnum GetActiveFrame() => currentFrame;
 
         /*      data        */
         WorkspaceController workspaceController;
+
         MenuController menuController;
+
         NewGateMenuController newGateMenuController;
+
         ModeManager modeManager = new ModeManager();
+
         Data.Scheme scheme;
+
         Constants.FrameEnum currentFrame;
+
         public Grid Grid { get; } = new Grid {
+
             HorizontalAlignment = HorizontalAlignment.Left,
             VerticalAlignment = VerticalAlignment.Top
         };
@@ -53,61 +63,89 @@ namespace SchemeCreator.UI {
         private void DotTappedEvent(object sender, DotTappedEventArgs e) {
             if(modeManager.CurrentMode == Constants.ModeEnum.addGateMode)
                 SwitchToFrame(Constants.FrameEnum.newGate, Grid);
+            scheme.dotController.lastTapped = e.dot;
         }
-        private void NewSchemeEvent(object sender, LastClickedButtonEventArgs e) =>
-            workspaceController.ShowDots(ref scheme);
-        private void LoadSchemeEvent(object sender, LastClickedButtonEventArgs e) => Data.Serializer.DeserializeAll(scheme);
-        private void SaveSchemeEvent(object sender, LastClickedButtonEventArgs e) => Data.Serializer.SerializeAll(scheme);
-        private void TraceSchemeEvent(object sender, LastClickedButtonEventArgs e) => Data.Tracing.ShowTracing(scheme);
-        private void WorkSchemeEvent(object sender, LastClickedButtonEventArgs e) {
-            // Clearing the elements data
-            foreach (var gate in from Data.Gate gate in scheme.gateController.gates
-                                 where gate.id != (byte)Constants.GateEnum.IN
-                                 select gate) { gate.outputValue = null; }
+        private void NewGateBtClickedEvent(object sender, NewGateBtClickedEventArgs e) {
+            scheme.gateController.gateInfo.Add(new Data.GateInfo(new Point(
+                scheme.dotController.lastTapped.Margin.Left - Constants.dotSize,
+                scheme.dotController.lastTapped.Margin.Top - Constants.dotSize),
+                e.type,
+                e.inputs));
 
-            // Sends signals from outputs to inputs
-            foreach (Line l in scheme.lineController.lines)
-                scheme.SendValue(scheme.GetValue(l), l);
+            SwitchToFrame(Constants.FrameEnum.workspace, Grid);
+            workspaceController.ShowDots(ref scheme);
         }
-        private void AddGateEvent(object sender, LastClickedButtonEventArgs e) {
+
+        private void NewSchemeEvent(object sender, LastClickedBtEventArgs e) =>
+            workspaceController.ShowDots(ref scheme);
+
+        private void LoadSchemeEvent(object sender, LastClickedBtEventArgs e) => Data.Serializer.DeserializeAll(scheme);
+
+        private void SaveSchemeEvent(object sender, LastClickedBtEventArgs e) => Data.Serializer.SerializeAll(scheme);
+
+        private void TraceSchemeEvent(object sender, LastClickedBtEventArgs e) => Data.Tracing.ShowTracing(scheme);
+
+        private void WorkSchemeEvent(object sender, LastClickedBtEventArgs e) {
+            // // Clearing the elements data
+            // foreach (var gate in from Data.Gate gate in scheme.gateController.gates
+            //                      where gate.type != Constants.GateEnum.IN
+            //                      select gate) { gate.outputValue = null; }
+
+            // // Sends signals from outputs to inputs
+            // foreach (Line l in scheme.lineController.lines)
+            //     scheme.SendValue(scheme.GetValue(l), l);
+        }
+        private void AddGateEvent(object sender, LastClickedBtEventArgs e) {
             menuController.InActivateModeButtons();
             e.button.BorderBrush = Constants.brushes[Constants.AccentEnum.light1];
             scheme.frameManager.modeManager.CurrentMode = Constants.ModeEnum.addGateMode;
         }
-        private void AddLineEvent(object sender, LastClickedButtonEventArgs e) {
+        private void AddLineEvent(object sender, LastClickedBtEventArgs e) {
             menuController.InActivateModeButtons();
             e.button.BorderBrush = Constants.brushes[Constants.AccentEnum.light1];
             scheme.frameManager.modeManager.CurrentMode = Constants.ModeEnum.addLineStartMode;
         }         
-        private void RemoveLineEvent(object sender, LastClickedButtonEventArgs e) {
+        private void RemoveLineEvent(object sender, LastClickedBtEventArgs e) {
             menuController.InActivateModeButtons();
             e.button.BorderBrush = Constants.brushes[Constants.AccentEnum.light1];
             scheme.frameManager.modeManager.CurrentMode = Constants.ModeEnum.removeLineMode;
         }
         
         public void SwitchToFrame(Constants.FrameEnum frame, Grid grid) {
+
             currentFrame = frame;
+            
             if(frame == Constants.FrameEnum.newGate) {
                 menuController.Hide();
                 workspaceController.Hide();
                 newGateMenuController.Show();
+                newGateMenuController.Update(Grid.Width, Grid.Height);
             }
             else {
                 newGateMenuController.Hide();
                 menuController.Show();
+                menuController.Update(Grid.Width, Grid.Height);
             }
         }
-        public void UpdateView(double width, double height) {
+
+        public void UpdateView() {
 
             System.Diagnostics.Debug.Assert(Grid.Width != 0);
             System.Diagnostics.Debug.Assert(Grid.Height != 0);
-
+            
             if (currentFrame == Constants.FrameEnum.newGate)
-                newGateMenuController.Update(width, height);
+                newGateMenuController.Update(Grid.Width, Grid.Height);
             else {
-                menuController.Update(width, height);
-                workspaceController.Update(width, height);
+                menuController.Update(Grid.Width, Grid.Height);
+                workspaceController.Update(Grid.Width, Grid.Height);
             }
+        }
+
+        public void SizeChanged(double width, double height) {
+            Grid.Width = width;
+            Grid.Height = height;   
+
+            UpdateView();         
         } 
     }
 }
