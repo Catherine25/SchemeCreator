3b39b7ce3a797191bab43418c322d45652e7e390
Catherine <Catherine25@users.noreply.github.com>
2019-03-26 23:32:26 +0200

    Add type; Remove id; Update GateInfo();

--- Data/GateInfo.cs
+++ Data/GateInfo.cs
@@ -4,15 +4,15 @@ using Windows.Foundation;
 namespace SchemeCreator.Data {
     [DataContract] public class GateInfo {
         [DataMember] public Point point;
-        [DataMember] public int id;
+        [DataMember] public Constants.GateEnum type;
         [DataMember] public int newGateInputs;
         [DataMember] public bool[] isInputsReserved;
 
         public GateInfo() { }
 
-        public GateInfo(Point p, int _id, int inputs) {
+        public GateInfo(Point p, Constants.GateEnum _type, int inputs) {
             point = p;
-            id = _id;
+            type = _type;
             newGateInputs = inputs;
             isInputsReserved = new bool[inputs];
         }
