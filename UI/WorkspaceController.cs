afacb66a6f9907a9a4ad5d27b4bbf7aa2a8947ef
Catherine <Catherine25@users.noreply.github.com>
2019-03-26 23:54:33 +0200

    Refactoring; Add ReloadGates(); Modify grid init;

--- UI/WorkspaceController.cs
+++ UI/WorkspaceController.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using Windows.UI.Input;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Shapes;
 
 namespace SchemeCreator.UI {
     class WorkspaceController : IFrameInterface {
+
         /*      methods      */
         public void SetParentGrid(Grid parentGrid) => parentGrid.Children.Add(grid);
+
         public void Hide() {
             grid.Children.Clear();
             isActive = false;
         }
+
         public void Update(double width, double height) {
             grid.Height = height;
             grid.Width = width;
         }
+
         public void ShowDots(ref Data.Scheme scheme) {
             scheme.dotController.InitNet(grid.ActualWidth, grid.ActualHeight);
             foreach(Ellipse e in scheme.dotController.dots) {
@@ -24,16 +29,36 @@ namespace SchemeCreator.UI {
             }
             isActive = true;
         }
+
+        public void ReloadGates(SchemeCreator.Data.Scheme scheme) {
+            foreach(Data.Gate gate in scheme.gateController.gates) {
+                grid.Children.Add(gate.DrawBody());
+                foreach (Ellipse ellipse in gate.DrawGateInputs())
+                    grid.Children.Add(ellipse);
+                foreach (Ellipse ellipse in gate.DrawGateOutputs())
+                    grid.Children.Add(ellipse);
+            }
+        }
+
         /*      events        */
+
         public delegate void DotTappedHandler(Ellipse sender, DotTappedEventArgs e);
+
         public event DotTappedHandler DotTappedEvent;
+
         public void DotTapped(object sender, TappedRoutedEventArgs e) =>
             DotTappedEvent(sender as Ellipse, new DotTappedEventArgs(sender as Ellipse));
 
         /*      data        */
+
         public bool IsActive { get => isActive; }
+
         bool isActive;
-        Grid grid = new Grid() { Margin = new Windows.UI.Xaml.Thickness(0, 60, 0, 0) };
+        
+        Grid grid = new Grid() { 
+            HorizontalAlignment = HorizontalAlignment.Left,
+            VerticalAlignment = VerticalAlignment.Top,
+            Margin = new Windows.UI.Xaml.Thickness(0, 60, 0, 0) };
     }
 
     class DotTappedEventArgs {
