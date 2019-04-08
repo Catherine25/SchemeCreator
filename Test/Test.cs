eb4dc23e5282b1a0f91c28d5580a2d7e28c3493c
Catherine <Catherine25@users.noreply.github.com>
2019-04-08 17:28:03 +0300

    Add: - scheme_frameManager_SwitchToFrame(); - scheme_frameManager_UpdateView(); - scheme_gatecontroller_gates_creation(); - scheme_gatecontroller_lineInfo_creation; - scheme();

--- Test/Test.cs
+++ Test/Test.cs
@@ -9,6 +9,7 @@ namespace SchemeCreator.Test {
             scheme_frameManager_SizeChanged();
             scheme_frameManager_creation();
         }
+
         public void scheme_creation() {
             
             var scheme = new SchemeCreator.Data.Scheme();  
@@ -52,6 +53,58 @@ namespace SchemeCreator.Test {
             Assert(scheme.frameManager.Grid.Height == 1000);
 
             System.Diagnostics.Debug.WriteLine("Test 3.2 scheme_frameManager passed");
+        }
+
+        public void scheme_frameManager_SwitchToFrame() {
+
+            var scheme = new SchemeCreator.Data.Scheme();
+
+            scheme.frameManager.SwitchToFrame(Constants.FrameEnum.newGate, scheme.frameManager.Grid);
+
+            Assert(scheme.frameManager.GetActiveFrame() == Constants.FrameEnum.newGate);
+            Assert(scheme.frameManager.GetActiveFrame() != Constants.FrameEnum.workspace);
+
+            scheme.frameManager.SwitchToFrame(Constants.FrameEnum.workspace, scheme.frameManager.Grid);
+
+            Assert(scheme.frameManager.GetActiveFrame() == Constants.FrameEnum.workspace);
+            Assert(scheme.frameManager.GetActiveFrame() != Constants.FrameEnum.newGate);
+
+            System.Diagnostics.Debug.WriteLine("Test 3.3 scheme_frameManager_SwitchToFrame passed");            
+        }
+
+        public void scheme_frameManager_UpdateView() {
+            var scheme = new SchemeCreator.Data.Scheme();
+
+            // TEST scheme.frameManager.UpdateView()
+
+            System.Diagnostics.Debug.WriteLine("Test 3.4 scheme_frameManager_UpdateView coming soon");            
+        }
+
+        public void scheme_gatecontroller_gates_creation() {
+
+            var scheme = new SchemeCreator.Data.Scheme();
+
+            Assert(scheme.gateController.gates != null);
+            Assert(scheme.gateController.gates.Count == 0);
+
+            System.Diagnostics.Debug.WriteLine("Test 4.1 scheme_gatecontroller_gates_creation passed");
+        }
+
+        public void scheme_gatecontroller_lineInfo_creation() {
+            
+            var scheme = new SchemeCreator.Data.Scheme();
+
+            Assert(scheme.lineController.wires != null);
+            Assert(scheme.lineController.wires.Count == 0);
+
+            System.Diagnostics.Debug.WriteLine("Test 4.2 scheme_gatecontroller_lineInfo_creation passed");
+        }
+
+        public void scheme() {
+
+            var menuController = new UI.MenuController();
+
+            //menuController
         }
     }
 }
