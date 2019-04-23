0b58be5026cbdc5f0c76507cd6f5b578ebfcd2ad
Catherine <Catherine25@users.noreply.github.com>
2019-04-23 12:14:43 +0300

    Delete ConnectionController

--- Data/ConnectionController.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-class ConnectionController {
-    Dictionary<string, string> gateComponentsConnection;
-    public void add(string gateComponent, string gateName) =>
-        gateComponentsConnection.Add(gateComponent, gateName);
-}
