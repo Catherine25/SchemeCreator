f9822a5bafc6dfc87b3a6440ebeb22142b7a2229
Catherine <Catherine25@users.noreply.github.com>
2019-09-07 18:52:09 +0300

    Add ButtonExtension

---
+++ Data/Extensions/ButtonExtension.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace SchemeCreator.Data.Extensions
+{
+    public static class ButtonExtension
+    {
+        public static Point CenterPoint(this Button button) => new Point
+        {
+            X = button.Margin.Left + button.Width / 2,
+            Y = button.Margin.Top + button.Height / 2
+        };
+    }
+}
