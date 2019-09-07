47a660a98b33065ad49849bfb2602a28564607e8
Catherine <Catherine25@users.noreply.github.com>
2019-09-07 19:00:56 +0300

    Add: - IsExternalInputsInited(); - IsAllConnected();

--- Data/Scheme.cs
+++ Data/Scheme.cs
@@ -1,4 +1,6 @@
-﻿namespace SchemeCreator.Data
+﻿using System.Collections.Generic;
+
+namespace SchemeCreator.Data
 {
     public class Scheme
     {
@@ -9,5 +11,51 @@
 
         //constructors
         public Scheme() => frameManager = new UI.FrameManager(this);
+
+        public bool IsExternalInputsInited()
+        {
+            IList<Gate> exINs = gateController.GetExternalInputs();
+
+            //check is all exINs are inited
+            for (int i = 0; i < exINs.Count; i++)
+                if (exINs[i].values[0] == null)
+                    return false;
+
+            return true;
+        }
+
+        public bool IsAllConnected()
+        {
+            int wLength = lineController.Wires.Count;
+            int gLength = gateController.Gates.Count;
+            int inputsCount = 0;
+
+            for (int g = 0; g < gLength; g++)
+            {
+                inputsCount += gateController.Gates[g].inputs;
+                inputsCount += gateController.Gates[g].outputs;
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < wLength; i++)
+            {
+                for (int j = 0; j < gLength; j++)
+                {
+                    Gate g = gateController.Gates[j];
+
+                    Wire w = lineController.Wires[i];
+
+                    if (g.WireConnects(w.start) || g.WireConnects(w.end))
+                        counter++;
+                }
+            }
+
+            if (inputsCount == counter)
+                return true;
+            else
+                return false;
+
+        }
     }
 }
