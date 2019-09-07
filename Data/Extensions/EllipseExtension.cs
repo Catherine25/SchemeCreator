f137bb790f47ac5e9eeaa30b077652c0a9cd3b20
Catherine <Catherine25@users.noreply.github.com>
2019-09-07 18:52:23 +0300

    Add EllipseExtension

---
+++ Data/Extensions/EllipseExtension.cs
@@ -0,0 +1,43 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Shapes;
+
+namespace SchemeCreator.Data.Extensions
+{
+    public static class EllipseExtension
+    {
+        public static Point CenterPoint(this Ellipse ellipse) => new Point
+        {
+            X = ellipse.Margin.Left + ellipse.Width / 2,
+            Y = ellipse.Margin.Top + ellipse.Height / 2
+        };
+
+        public static void IncreaseSize(this Ellipse ellipse)
+        {
+            Point center = ellipse.CenterPoint();
+
+            ellipse.Width *= 2;
+            ellipse.Height *= 2;
+
+            ellipse.Margin = new Thickness
+            {
+                Left = center.X - ellipse.Width / 2,
+                Top = center.Y - ellipse.Height / 2
+            };
+        }
+
+        public static void DecreaseSize(this Ellipse ellipse)
+        {
+            Point center = ellipse.CenterPoint();
+
+            ellipse.Width /= 2;
+            ellipse.Height /= 2;
+
+            ellipse.Margin = new Thickness
+            {
+                Left = center.X - ellipse.Width / 2,
+                Top = center.Y - ellipse.Height / 2
+            };
+        }
+    }
+}
