fd2f1fdd17ba85b67d29befd32f433e1bc4674a1
Catherine <Catherine25@users.noreply.github.com>
2019-09-13 12:36:33 +0300

    Add SetStandartAlighnment()

--- Data/Extensions/GridExtension.cs
+++ Data/Extensions/GridExtension.cs
@@ -12,5 +12,10 @@ namespace SchemeCreator.Data.Extensions
         }
         public static Size GetSize(this Grid grid) => new Size(grid.Width, grid.Height);
         public static Size GetActualSize(this Grid grid) => new Size(grid.ActualWidth, grid.ActualHeight);
+        public static void SetStandartAlighnment(this Grid grid)
+        {
+            grid.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+            grid.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+        }
     }
 }
