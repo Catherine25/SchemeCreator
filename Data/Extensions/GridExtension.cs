5d8d22e7accf786ab7fea6fd1cfd97bffc4a553e
Catherine <Catherine25@users.noreply.github.com>
2019-09-13 13:42:41 +0300

    Add: - SetRect; - GetSize; - GetLeftTop; Remove: - SetSize; - GetRect;

--- Data/Extensions/GridExtension.cs
+++ Data/Extensions/GridExtension.cs
@@ -5,17 +5,32 @@ namespace SchemeCreator.Data.Extensions
 {
     public static class GridExtension
     {
-        public static void SetSize(this Grid grid, Size size)
+        public static void SetRect(this Grid grid, Rect rect)
         {
-            grid.Width = size.Width;
-            grid.Height = size.Height;
+            grid.Margin = new Windows.UI.Xaml.Thickness
+            {
+                Left = rect.Left,
+                Top = rect.Top
+            };
+            grid.Width = rect.Width;
+            grid.Height = rect.Height;
         }
-        public static Size GetSize(this Grid grid) => new Size(grid.Width, grid.Height);
+        public static Rect GetRect(this Grid grid) => new Rect
+        {
+            X = grid.Margin.Right,
+            Y = grid.Margin.Top,
+            Width = grid.Width,
+            Height = grid.Height
+        };
+
         public static Size GetActualSize(this Grid grid) => new Size(grid.ActualWidth, grid.ActualHeight);
         public static void SetStandartAlighnment(this Grid grid)
         {
             grid.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
             grid.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
         }
+        public static Point GetLeftTop(this Grid grid) =>
+            new Point(grid.Margin.Left, grid.Margin.Top);
+
     }
 }
