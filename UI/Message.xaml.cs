8e1ddf6001a6f141bdd1f15880c3fe25173dce24
Catherine <Catherine25@users.noreply.github.com>
2019-09-13 12:44:08 +0300

    Add: - choosedGate; - inputs; - outputs; Rename textBlock;

--- UI/Message.xaml.cs
+++ UI/Message.xaml.cs
@@ -8,6 +8,9 @@ namespace SchemeCreator.UI
 {
     public sealed partial class Message : ContentDialog
     {
+        public Constants.GateEnum choosedGate;
+        public int inputs, outputs = 1;
+
         public Message(Constants.MessageTypes mt)
         {
             InitializeComponent();
@@ -15,9 +18,10 @@ namespace SchemeCreator.UI
             string[] messageInfo = Text.GetText(mt);
 
             Title = messageInfo[0];
-            TextBlock.Text = messageInfo[1];
             PrimaryButtonText = messageInfo[2];
             SecondaryButtonText = messageInfo[3];
+
+            textBlock.Text = messageInfo[1];
         }
 
         public Message(Constants.GateEnum type)
@@ -26,14 +30,14 @@ namespace SchemeCreator.UI
 
             Data.Gate gate = new Data.Gate(type, 2, 1, new Point(0.0, 0.0));
 
-            if(type == Constants.GateEnum.IN) {
-                TextBlock.Text =
+            if (type == Constants.GateEnum.IN) {
+                textBlock.Text =
                     "It's an external input.\n" +
                     "You can arrange 'False' or 'True' value by clicking on it.\n" +
                     "By default it's null.";
             }
             else if(type == Constants.GateEnum.OUT) {
-                TextBlock.Text =
+                textBlock.Text =
                     "It's an external output.\n" +
                     "It gets it's value via signals visualisation.\n" +
                     "You can try it clicking on 'Work' button.";
@@ -42,33 +46,33 @@ namespace SchemeCreator.UI
             {
                 gate.values[0] = false;
                 gate.Work();
-                TextBlock.Text = "False" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text = "False" + "\t|\t" + gate.values[0] + "\n";
 
                 gate.values[0] = true;
                 gate.Work();
-                TextBlock.Text = "False" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text = "False" + "\t|\t" + gate.values[0] + "\n";
             }
             else
             {
                 gate.values[0] = false;
                 gate.values[1] = false;
                 gate.Work();
-                TextBlock.Text = "False" + "\t" + "False" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text = "False" + "\t" + "False" + "\t|\t" + gate.values[0] + "\n";
 
                 gate.values[0] = false;
                 gate.values[1] = true;
                 gate.Work();
-                TextBlock.Text += "False" + "\t" + "True" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text += "False" + "\t" + "True" + "\t|\t" + gate.values[0] + "\n";
 
                 gate.values[0] = true;
                 gate.values[1] = false;
                 gate.Work();
-                TextBlock.Text += "True" + "\t" + "False" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text += "True" + "\t" + "False" + "\t|\t" + gate.values[0] + "\n";
 
                 gate.values[0] = true;
                 gate.values[1] = true;
                 gate.Work();
-                TextBlock.Text += "True" + "\t" + "True" + "\t|\t" + gate.values[0] + "\n";
+                textBlock.Text += "True" + "\t" + "True" + "\t|\t" + gate.values[0] + "\n";
             }
 
             PrimaryButtonText = "Close";
