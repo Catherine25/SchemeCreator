102a964defe345c7125f81547d4ab75341a72453
Catherine <Catherine25@users.noreply.github.com>
2019-12-23 00:03:09 +0200

    Add LineExtension.cs

---
+++ Data/Extensions/LineExtension.cs
@@ -0,0 +1,13 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml.Shapes;
+
+namespace SchemeCreator.Data.Extensions
+{
+    public static class LineExtension
+    {
+        public static Point GetStartPoint(this Line line) =>
+            new Point(line.X1, line.Y1);
+        public static Point GetEndPoint(this Line line) =>
+            new Point(line.X2, line.Y2);
+    }
+}
