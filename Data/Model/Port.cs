a56d3c23cb10dd04a84fe410775d7096fef61ff6
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-05-10 02:59:30 +0300

    Create Port.cs

---
+++ Data/Model/Port.cs
@@ -0,0 +1,86 @@
+using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+using static SchemeCreator.Data.ConstantsNamespace.Constants;
+
+namespace SchemeCreator.Data.Model
+{
+    public class Port : IResizable
+    {
+        #region Fields
+
+        private readonly ConnectionType type;
+
+        public bool? BooleanValue
+        {
+            get { return booleanValue; }
+            set
+            {
+                booleanValue = value;
+
+                if (booleanValue == true)
+                    ellipse.Fill = brushes[AccentEnum.light1];
+                else if (booleanValue == false)
+                    ellipse.Fill = brushes[AccentEnum.dark1];
+                else
+                    ellipse.Fill = brushes[AccentEnum.accent2];
+            }
+        }
+        private bool? booleanValue;
+
+        public Point CenterPoint { get; set; }
+        
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+                ellipse.Width = size.Width;
+                ellipse.Height = size.Height;
+                ellipse.Margin = new Thickness
+                {
+                    Left = CenterPoint.X - size.Width / 2,
+                    Top = CenterPoint.Y - size.Height / 2
+                };
+            }
+        }
+        private Size size;
+
+        private Ellipse ellipse;
+
+        #endregion
+
+        #region Events
+
+        public event Action<Port> Tapped;
+
+        #endregion
+        
+        public Port(ConnectionType connectionType)
+        {
+            type = connectionType;
+
+            ellipse = new Ellipse
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+
+            ellipse.Tapped += (object sender, TappedRoutedEventArgs e) => Tapped(this);
+            ellipse.PointerEntered += (object sender, PointerRoutedEventArgs e) => ChangeSize(true);
+            ellipse.PointerExited += (object sender, PointerRoutedEventArgs e) => ChangeSize(false);
+        }
+
+        public void ChangeSize(bool increase) =>
+            Size = increase ? new Size(Size.Width * 2, Size.Height * 2) : new Size(Size.Width / 2, Size.Height / 2);
+
+        public void SetParentGrid(Grid grid) => grid.Children.Add(this.ellipse);
+    }
+}
