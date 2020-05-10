77b299742d72c8ae33ebacde5e695bf7694e67e9
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-05-10 15:24:26 +0300

    Move coloring to Colorer
    Some refactoring

--- Data/Models/Wire.cs
+++ Data/Models/Wire.cs
@@ -1,50 +1,40 @@
+using SchemeCreator.Data.Services;
 using System.Runtime.Serialization;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
-using SchemeCreator.Data.ConstantsNamespace;
+using static SchemeCreator.Data.Constants;
 
 namespace SchemeCreator.Data.Models
 {
-    [DataContract] public class Wire {
-    [DataMember] public Point start, end;
-    [DataMember] public bool? isActive;
+    [DataContract]
+    public class Wire
+    {
+        [DataMember]
+        public Point start, end;
+        [DataMember]
+        public bool? isActive;
 
-    public Line CreateLine() {
+        public Line CreateLine()
+        {
 
-        Line l = new Line {
-            X1 = start.X,
-            X2 = end.X,
-            Y1 = start.Y,
-            Y2 = end.Y,
-            StrokeThickness =
-                Constants.wireThickness,
-            StrokeEndLineCap = PenLineCap.Round,
-            StrokeStartLineCap = PenLineCap.Round,
-            HorizontalAlignment = HorizontalAlignment.Left,
-            VerticalAlignment = VerticalAlignment.Top
-        };
+            Line l = new Line
+            {
+                X1 = start.X,
+                X2 = end.X,
+                Y1 = start.Y,
+                Y2 = end.Y,
+                StrokeThickness = wireThickness,
+                StrokeEndLineCap = PenLineCap.Round,
+                StrokeStartLineCap = PenLineCap.Round,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top
+            };
 
-            if (isActive == true)
-            {
-                l.Stroke =
-                    Constants.brushes[Constants.AccentEnum.light1];
-                l.Fill = Constants.brushes[Constants.AccentEnum.dark1];
-            }
-            else if (isActive == false)
-            {
-                l.Stroke =
-                Constants.brushes[Constants.AccentEnum.dark1];
-                l.Fill = Constants.brushes[Constants.AccentEnum.light1];
-            }
-            else
-            {
-                l.Stroke = Constants.brushes[Constants.AccentEnum.accent2];
-                l.Fill = Constants.brushes[Constants.AccentEnum.accent2];
-            }
-        
-        return l;
+            Colorer.SetFillByValue(l, isActive);
+
+            return l;
         }
 
         public Point Center => new Point
