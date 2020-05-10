3734243fa5cccda767c573a82429b67831ca73fb
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-05-10 15:19:11 +0300

    Use Colorer

--- Data/Controllers/DotController.cs
+++ Data/Controllers/DotController.cs
@@ -3,6 +3,8 @@ using Windows.UI.Xaml.Shapes;
 using SchemeCreator.Data.Extensions;
 using Windows.Foundation;
 using static SchemeCreator.Data.Constants;
+using Windows.UI.Xaml.Media;
+using SchemeCreator.Data.Services;
 
 namespace SchemeCreator.Data.Controllers
 {
@@ -21,13 +23,14 @@ namespace SchemeCreator.Data.Controllers
         public void InitNet(Size actSize)
         {
             double stepW = actSize.Width / (netSize + 1),
+                stepH = actSize.Height / (netSize + 1);
 
             for (int i = 1; i <= netSize; i++)
                 for (int j = 1; j <= netSize; j++)
                 {
-                    Ellipse ellipse = new Ellipse { 
-                        Fill = Constants.brushes[Constants.AccentEnum.dark1]
-                    };
+                    Ellipse ellipse = new Ellipse();
+
+                    Colorer.SetFillByValue(ellipse, false);
 
                     ellipse.SetSizeAndCenterPoint(dotSize, new Point(stepW * i, stepH * j));
 
