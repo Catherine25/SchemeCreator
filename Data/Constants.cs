7fe6d97861993c5498675f47e05e143ea45582f9
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-05-10 15:13:52 +0300

    Move to SchemeCreator.Data

--- Data/Constants.cs
+++ Data/Constants.cs
@@ -3,7 +3,8 @@ using Windows.UI.ViewManagement;
 using System.Collections.Generic;
 using Windows.Foundation;
 
-namespace SchemeCreator.Data.ConstantsNamespace {
+namespace SchemeCreator.Data
+{
     static public class Constants
     {
         public static Size externalGateSize = new Size { Width = 50, Height = 50 };
@@ -66,27 +67,10 @@ namespace SchemeCreator.Data.ConstantsNamespace {
             { BtId.changeValueBt, "Change Value" }
         };
 
-        public static Dictionary <AccentEnum, SolidColorBrush> brushes = new Dictionary<AccentEnum, SolidColorBrush> {
-            { AccentEnum.accent, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.Accent)) },
-            { AccentEnum.background, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.Background)) },
-            { AccentEnum.foreground, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.Foreground)) },
-
-            { AccentEnum.dark1, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentDark1)) },
-            { AccentEnum.dark2, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentDark2)) },
-            { AccentEnum.dark3, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentDark3)) },
-
-            { AccentEnum.light1, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentLight1)) },
-            { AccentEnum.light2, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentLight2)) },
-            { AccentEnum.light3, new SolidColorBrush(new UISettings().GetColorValue(UIColorType.AccentLight3)) },
-
-            { AccentEnum.accent2, new SolidColorBrush(Windows.UI.Colors.Maroon) }
-        };
-
         public enum BtId { newSchemeBt, loadSchemeBt, saveSchemeBt, traceSchemeBt, workSchemeBt, addLineBt, changeValueBt }
         public enum GateEnum { IN, OUT, Buffer, NOT, AND, NAND, OR, NOR, XOR, XNOR };
         public enum ModeEnum { addGateMode, addLineMode, changeValueMode }
         public enum FrameEnum { workspace, newGate }
-        public enum AccentEnum { accent, dark1, dark2, dark3, light1, light2, light3, background, foreground, accent2 }
         public enum ComponentTypeEnum { gate, wire }
         public enum MessageTypes { exInsNotInited, gatesNotConnected, functionIsNotSupported, modeChanged, newSchemeButtonClicked, detailedView, createGate, visualizingFailed }
         public enum MessageAttribute { title, text, button1, button2 }
