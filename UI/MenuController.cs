7a0c3bea7ee9f714f9c6bd3269b86fb011954ca9
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-06-14 18:03:30 +0300

    Refactoring

--- UI/MenuController.cs
+++ UI/MenuController.cs
@@ -6,6 +6,7 @@ using Windows.UI.Xaml.Controls;
 using SchemeCreator.Data.Extensions;
 using static SchemeCreator.Data.Constants;
 using SchemeCreator.Data.Services;
+using System.Linq;
 
 namespace SchemeCreator.UI
 {
@@ -14,15 +15,18 @@ namespace SchemeCreator.UI
         Dictionary<BtId, Button> buttons = new Dictionary<BtId, Button>();
         Grid grid = new Grid();
 
-        /*      events      */
-        public event Action NewSchemeBtClickEvent,
-            LoadSchemeBtClickEvent,
-            SaveSchemeBtClickEvent,
-            TraceSchemeBtClickEvent,
-            WorkSchemeBtClickEvent;
+        #region Events
+
+        public event Action NewSchemeBtClickEvent;
+        public event Action LoadSchemeBtClickEvent;
+        public event Action SaveSchemeBtClickEvent;
+        public event Action TraceSchemeBtClickEvent;
+        public event Action WorkSchemeBtClickEvent;
+
         public event Action<BtId> ChangeModeEvent;
 
-        /*      constructor     */
+        #endregion
+
         public MenuController()
         {
             grid.SetStandartAlighnment();
@@ -35,62 +39,50 @@ namespace SchemeCreator.UI
 
                 buttons.Add(id, button);
             }
+
             EventSubscribe();
         }
         
-        /*      events      */
-        private void EventSubscribe() {
-            buttons[BtId.newSchemeBt].Click += NewSchemeBtClick;
-            buttons[BtId.loadSchemeBt].Click += LoadSchemeBtClick;
-            buttons[BtId.saveSchemeBt].Click += SaveSchemeBtClick;
-            buttons[BtId.traceSchemeBt].Click += TraceSchemeBtClick;
-            buttons[BtId.workSchemeBt].Click += WorkSchemeBtClick;
-            buttons[BtId.addLineBt].Click += AddLineBtClick;
-            buttons[BtId.changeValueBt].Click += ChangeValueBtClick;
+        private void EventSubscribe()
+        {
+            buttons[BtId.newSchemeBt].Click += (object o, RoutedEventArgs e) => NewSchemeBtClickEvent();
+            buttons[BtId.loadSchemeBt].Click += (object o, RoutedEventArgs e) => LoadSchemeBtClickEvent();
+            buttons[BtId.saveSchemeBt].Click += (object o, RoutedEventArgs e) => SaveSchemeBtClickEvent();
+            buttons[BtId.traceSchemeBt].Click += (object o, RoutedEventArgs e) => TraceSchemeBtClickEvent();
+            buttons[BtId.workSchemeBt].Click += (object o, RoutedEventArgs e) => WorkSchemeBtClickEvent();
+            buttons[BtId.addLineBt].Click += (object o, RoutedEventArgs e) => ChangeModeEvent(BtId.addLineBt);
+
+            buttons[BtId.changeValueBt].Click += (object o, RoutedEventArgs e) => {
+                ChangeModeEvent(BtId.changeValueBt);
+                Colorer.ColorMenuButtonBorderByValue(o as Button, true); };
         }
 
-        private void NewSchemeBtClick(object sender, RoutedEventArgs e) =>
-            NewSchemeBtClickEvent();
-        private void LoadSchemeBtClick(object sender, RoutedEventArgs e) =>
-            LoadSchemeBtClickEvent();
-        private void SaveSchemeBtClick(object sender, RoutedEventArgs e) =>
-            SaveSchemeBtClickEvent();
-        private void TraceSchemeBtClick(object sender, RoutedEventArgs e) =>
-            TraceSchemeBtClickEvent();
-        private void WorkSchemeBtClick(object sender, RoutedEventArgs e) =>
-            WorkSchemeBtClickEvent();
-        private void AddLineBtClick(object sender, RoutedEventArgs e) {
-            ChangeModeEvent(BtId.addLineBt);
-            Colorer.ColorMenuButtonBorderByValue(sender as Button, true);
-        }       
-        private void ChangeValueBtClick (object sender, RoutedEventArgs e) {
-            ChangeModeEvent(BtId.changeValueBt);
-            Colorer.ColorMenuButtonBorderByValue(sender as Button, true);
-        }
+        #region Public methods
 
-        /*      methods     */
-        public void Update(Rect rect) {
+        public void Update(Rect rect)
+        {
             int i = 0;
-            foreach (Button button in buttons.Values) {
+            foreach (Button button in buttons.Values)
+            {
                 button.Height = rect.Height;
-                button.Width = rect.Width / (buttons.Count);
-                button.Margin = new Thickness((i*button.Width), 0, 0, 0); 
+                button.Width = rect.Width / buttons.Count;
+                button.Margin = new Thickness(i * button.Width, 0, 0, 0);
                 i++;
             }
         }
 
         public void SetParentGrid(Grid parentGrid) => parentGrid.Children.Add(grid);
-        
-        public void Show() {
-            foreach (Button button in buttons.Values)
-                grid.Children.Add(button);
-        }
-        
+
+        public void Show() => buttons.Values.ToList().ForEach(b => grid.Children.Add(b));
+
         public void Hide() => grid.Children.Clear();
         
-        public void InActivateModeButtons() {
+        public void InActivateModeButtons()
+        {
             Colorer.ColorMenuButtonBorderByValue(buttons[BtId.addLineBt] as Button, false);
             Colorer.ColorMenuButtonBorderByValue(buttons[BtId.changeValueBt] as Button, false);
         }
+
+        #endregion
     }
 }
