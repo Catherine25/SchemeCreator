f8ce09223b911d18bb7c301ce7f1d7d616451221
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-06-20 22:21:31 +0300

    SC-31: Change serialization to JSON

--- Data/Services/Serializer.cs
+++ Data/Services/Serializer.cs
@@ -1,124 +1,24 @@
-﻿using SchemeCreator.Data.Models;
+﻿using Newtonsoft.Json;
+using SchemeCreator.Data.Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
 using Windows.Storage;
 
 namespace SchemeCreator.Data.Services
 {
-    internal static class Serializer
+    static class Serializer
     {
-        //data
-        public const string gatePath = "gateData.txt", linePath = "lineData.txt";
+        public const string gatePath = "gateData.txt";
+        public const string linePath = "lineData.txt";
 
-        public static List<string> serializeGateData, serializeLineData;
+        public static List<string> serializeGateData;
+        public static List<string> serializeLineData;
 
         static Serializer()
         {
             serializeGateData = new List<string>();
             serializeLineData = new List<string>();
-        }
-
-        public static void SerializeAll(Scheme scheme)
-        {
-            int gateCount = scheme.gateController.Gates.Count;
-
-            for (int i = 0; i < gateCount; i++)
-                serializeGateData.Add(scheme.gateController.Gates[i].SerializeGate());
-
-            int wireCount = scheme.lineController.Wires.Count;
-
-            for (int i = 0; i < wireCount; i++)
-                serializeGateData.Add(scheme.lineController.Wires[i].SerializeLine());
-        }
-
-        public static void DeserializeAll(Scheme scheme)
-        {
-            scheme.gateController.Gates.Clear();
-            scheme.lineController.Wires.Clear();
-
-            foreach (string s in serializeGateData)
-                scheme.gateController.Gates.Add(s.DeserializeGate());
-
-            foreach (string s in serializeLineData)
-                scheme.lineController.Wires.Add(s.DeserializeLine());
-        }
-
-        //Gate and line serialization and serialization
-        public static string SerializeGate(this Gate gi)
-        {
-            var ms = new MemoryStream();
-            // Write an object to the Stream and leave it opened
-            using (var writer = XmlDictionaryWriter.CreateTextWriter(ms, Encoding.UTF8, ownsStream: false))
-            {
-                var ser = new DataContractSerializer(typeof(Gate));
-                ser.WriteObject(writer, gi);
-            }
-            // Read serialized string from Stream and close it
-            using (var reader = new StreamReader(ms, Encoding.UTF8))
-            {
-                ms.Position = 0;
-                return reader.ReadToEnd();
-            }
-        }
-
-        public static string SerializeLine(this Wire w)
-        {
-            var ms = new MemoryStream();
-            // Write an object to the Stream and leave it opened
-            using (var writer = XmlDictionaryWriter.CreateTextWriter(ms, Encoding.UTF8, ownsStream: false))
-            {
-                var ser = new DataContractSerializer(typeof(Wire));
-                ser.WriteObject(writer, w);
-            }
-            // Read serialized string from Stream and close it
-            using (var reader = new StreamReader(ms, Encoding.UTF8))
-            {
-                ms.Position = 0;
-                return reader.ReadToEnd();
-            }
-        }
-
-        public static Gate DeserializeGate(this string xml)
-        {
-            var ms = new MemoryStream();
-            // Write xml content to the Stream and leave it opened
-            using (var writer = new StreamWriter(ms, Encoding.UTF8, 512, leaveOpen: true))
-            {
-                writer.Write(xml);
-                writer.Flush();
-                ms.Position = 0;
-            }
-            // Read Stream to the Serializer and Deserialize and close it
-            using (var reader = XmlDictionaryReader.CreateTextReader(ms, Encoding.UTF8,
-            new XmlDictionaryReaderQuotas(), null))
-            {
-                var ser = new DataContractSerializer(typeof(Gate));
-                return (Gate)ser.ReadObject(reader);
-            }
-        }
-
-        public static Wire DeserializeLine(this string xml)
-        {
-            var ms = new MemoryStream();
-            // Write xml content to the Stream and leave it opened
-            using (var writer = new StreamWriter(ms, Encoding.UTF8, 512, leaveOpen: true))
-            {
-                writer.Write(xml);
-                writer.Flush();
-                ms.Position = 0;
-            }
-            // Read Stream to the Serializer and Deserialize and close it
-            using (var reader = XmlDictionaryReader.CreateTextReader(ms, Encoding.UTF8,
-            new XmlDictionaryReaderQuotas(), null))
-            {
-                var ser = new DataContractSerializer(typeof(Wire));
-                return (Wire)ser.ReadObject(reader);
-            }
         }
 
         public static async Task Load(Scheme scheme)
@@ -131,36 +31,34 @@ namespace SchemeCreator.Data.Services
             if (gateFile == null)
                 await folder.CreateFileAsync(gatePath);
 
-            serializeGateData.AddRange(await FileIO.ReadLinesAsync(gateFile));
+            scheme.gateController.Gates.Clear();
+            scheme.gateController.Gates = JsonConvert.DeserializeObject<List<Gate>>(await FileIO.ReadTextAsync(gateFile));
 
-            //serialization of lines
+            //serialization of wires
             StorageFile lineFile = await folder.GetFileAsync(linePath);
 
             if (lineFile == null)
                 await folder.CreateFileAsync(linePath);
 
-            serializeLineData.AddRange(await FileIO.ReadLinesAsync(lineFile));
-
-            DeserializeAll(scheme);
+            scheme.lineController.Wires.Clear();
+            scheme.lineController.Wires = JsonConvert.DeserializeObject<List<Wire>>(await FileIO.ReadTextAsync(lineFile));
         }
 
         public static async Task Save(Scheme scheme)
         {
-            SerializeAll(scheme);
-
             StorageFolder folder = KnownFolders.PicturesLibrary;
 
             //serialization of gates
             StorageFile gateFile = await folder.CreateFileAsync(gatePath,
                                                 CreationCollisionOption.OpenIfExists);
 
-            await FileIO.WriteLinesAsync(gateFile, serializeGateData);
+            await FileIO.WriteTextAsync(gateFile, JsonConvert.SerializeObject(scheme.gateController.Gates));
 
-            //serialization of lines
+            //serialization of wires
             StorageFile lineFile = await folder.CreateFileAsync(linePath,
                                               CreationCollisionOption.OpenIfExists);
 
-            await FileIO.WriteLinesAsync(lineFile, serializeLineData);
+            await FileIO.WriteTextAsync(lineFile, JsonConvert.SerializeObject(scheme.lineController.Wires));
         }
     }
 }
