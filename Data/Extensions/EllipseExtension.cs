105d362373eb44a01503471f51248fbcdcb44035
Catherine <catherinesolodoukhina@gmail.com>
2021-05-12 23:56:49 +0300

    Change activation type

--- Data/Extensions/EllipseExtension.cs
+++ Data/Extensions/EllipseExtension.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using SchemeCreator.Data.Services;
+using System.Diagnostics;
 using System.Numerics;
 using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Shapes;
 
@@ -8,19 +10,7 @@ namespace SchemeCreator.Data.Extensions
 {
     public static class EllipseExtension
     {
-        //private static Thickness CalculateMargin(Vector3 center, Size size) => new Thickness
-        //{
-        //    Left = center.X - size.Width / 2,
-        //    Top = center.Y - size.Height / 2
-        //};
-
-        //public static Point GetCenterPoint(this Ellipse ellipse) => new Point
-        //{
-        //    X = ellipse.Margin.Left + ellipse.Width / 2,
-        //    Y = ellipse.Margin.Top + ellipse.Height / 2
-        //};
-
-        public static Size GetSize(this Ellipse ellipse) => new Size(ellipse.Width, ellipse.Height);
+        public static Size GetSize(this Ellipse ellipse) => new(ellipse.Width, ellipse.Height);
 
         public static void SetSize(this Ellipse ellipse, Size size)
         {
@@ -28,45 +18,9 @@ namespace SchemeCreator.Data.Extensions
             ellipse.Height = size.Height;
         }
 
-        //public static void SetSizeAndCenterPoint(this Ellipse ellipse, Size? size, Vector3? center)
-        //{
-        //    if (size == null)
-        //        size = GetSize(ellipse);
-
-        //    ellipse.CenterPoint = center;
-
-        //    ellipse.SetSize(size.Value);
-
-        //    //ellipse.Margin = CalculateMargin(center.Value, size.Value);
-
-        //    Debug.WriteLine($"Ellipse Margin = {ellipse.Margin}");
-        //    Debug.WriteLine($"Ellipse Size = {ellipse.GetSize()}");
-        //    Debug.WriteLine($"Ellipse CenterPoint = {ellipse.CenterPoint}");
-        //}
-
-        public static void IncreaseSize(this Ellipse ellipse)
-        {
-            //Vector3 center = ellipse.CenterPoint;
-
-            ellipse.Width *= 2;
-            ellipse.Height *= 2;
-
-            //ellipse.Margin = CalculateMargin(center, GetSize(ellipse));
-
-            Debug.WriteLine($"Ellipse Margin = {ellipse.Margin}");
-            Debug.WriteLine($"Ellipse Size = {ellipse.GetSize()}");
-            Debug.WriteLine($"Ellipse CenterPoint = {ellipse.CenterPoint}");
-        }
-
-        public static void DecreaseSize(this Ellipse ellipse)
-        {
-            //Vector3 center = ellipse.CenterPoint;
-
-            ellipse.Width /= 2;
-            ellipse.Height /= 2;
-
-            //ellipse.Margin = CalculateMargin(center, GetSize(ellipse));
-        }
+        public static void Activate(this Ellipse ellipse) => ellipse.Stroke = Colorer.ActivatedColor;
+        public static void Deactivate(this Ellipse ellipse) => ellipse.Stroke = Colorer.DeactivatedColor;
+        
 
         public static void SetStandartAlingment(this Ellipse ellipse)
         {
